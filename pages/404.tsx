421281a94160e16e85f9de8b919eb926211d97ca
Marciel404 <48138111+eumarciel404@users.noreply.github.com>
2026-08-27 15:08:24 -0300

    PAdronizando nome

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 
-export default function Home() {
+export default function Error404() {
     return (
         <>
             <Head>
