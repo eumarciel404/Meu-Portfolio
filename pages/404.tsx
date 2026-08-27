ed4761caedccc1167fdf1248096ece9bcfb9c1c6
Marciel404 <48138111+eumarciel404@users.noreply.github.com>
2026-08-27 09:47:01 -0300

    Adicionando pagina de erro personalizado

---
+++ pages/404.tsx
@@ -0,0 +1,90 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Error 404</title>
+                <meta
+                    name="description"
+                    content="PUTZZZZZZ.... PaGiNa NãO EnCoNtRaDa"
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/gifs/nyan_cat.gif" />
+            </Head>
+            <body>
+                <audio id="errorsong" src={"/songs/errorsong.mp3"} loop />
+                <div
+                    style={{
+                        backgroundColor: "black",
+                    }}
+                >
+                    <div
+                        id="buttonTriggerPlay"
+                        style={{
+                            height: "100vh",
+                            width: "100vw",
+                            display: "flex",
+                            justifyContent: "center",
+                            placeContent: "center",
+                        }}
+                    >
+                        <div>
+                            <button
+                                style={{
+                                    paddingTop: 15,
+                                    paddingLeft: 25,
+                                    paddingBottom: 15,
+                                    paddingRight: 25,
+                                    backgroundColor: "#00002e",
+                                    border: "2px solid grey",
+                                    borderRadius: 5,
+                                }}
+                                onClick={() => {
+                                    const audio = document.getElementById(
+                                        "errorsong",
+                                    )! as HTMLAudioElement;
+                                    audio.play();
+                                    document.getElementById(
+                                        "errorPage",
+                                    )!.style.display = "flex";
+                                    document.getElementById(
+                                        "buttonTriggerPlay",
+                                    )!.style.display = "none";
+                                }}
+                            >
+                                ?
+                            </button>
+                        </div>
+                    </div>
+                    <div id="errorPage">
+                        <div>
+                            <Image
+                                src={"/gifs/nyan_cat.gif"}
+                                width={150}
+                                height={150}
+                                alt="ErrorGif"
+                                style={{
+                                    borderRadius: 99,
+                                }}
+                            />
+                        </div>
+                        <div>
+                            <h1>Error404 Page Not Found</h1>
+                            <p>
+                                PUTZZZZZZZ.... Deu ERRO AqUi oh Pagina NãO
+                                EnCoNtRaDa, isso quer dizer que você entrou em
+                                uma pagina que não existe nos arquivos do meu
+                                site
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </body>
+        </>
+    );
+}
