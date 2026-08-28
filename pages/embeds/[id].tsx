0d5bf3c88293d22c1e3d6a70819abc61c34592dd
Marciel404 <48138111+eumarciel404@users.noreply.github.com>
2026-08-27 23:19:04 -0300

    Corrigindo erro de não renderizar pros bots

--- pages/embeds/[id].tsx
+++ pages/embeds/[id].tsx
@@ -1,115 +1,112 @@
-import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
 import axios from "axios";
 import Head from "next/head";
-import Error404 from "../404";
 import Image from "next/image";
 
-export default function Id() {
-    const router = useRouter();
-    const { id } = router.query;
-    const [data, setData] = useState<any>(null);
-
-    useEffect(() => {
-        if (id) {
-            axios
-                .get("/api/teste")
-                .then((x) => setData(x.data.response.find((i: any) => i.id === id)))
-                .catch((err) => console.error(err));
-        }
-    }, [id]);
-
-    if (data) {
-        return (
-            <>
-                <Head>
-                    <title>?</title>
-                    <meta name="og:title" content={data["og:title"]} />
-                    <meta
-                        name="og:description"
-                        content={data["og:description"]}
-                    />
-                    <meta name="og:image" content={data["og:image"]} />
-                    <meta name="og:url" content={data["og:url"]} />
-                </Head>
-                <body>
-                    <audio id="errorsong" src={"/songs/errorsong.mp3"} loop />
+export default function Id(data: any) {
+    return (
+        <>
+            <Head>
+                <title>?</title>
+                <meta property="og:title" content={data["og:title"] || ""} />
+                <meta
+                    property="og:description"
+                    content={data["og:description"] || ""}
+                />
+                <meta property="og:image" content={data["og:image"] || ""} />
+                <meta property="og:url" content={data["og:url"] || ""} />
+            </Head>
+            <body>
+                <audio id="errorsong" src={"/songs/errorsong.mp3"} loop />
+                <div
+                    style={{
+                        backgroundColor: "black",
+                    }}
+                >
                     <div
+                        id="buttonTriggerPlay"
                         style={{
-                            backgroundColor: "black",
+                            height: "100vh",
+                            width: "100vw",
+                            display: "flex",
+                            justifyContent: "center",
+                            placeContent: "center",
                         }}
                     >
-                        <div
-                            id="buttonTriggerPlay"
-                            style={{
-                                height: "100vh",
-                                width: "100vw",
-                                display: "flex",
-                                justifyContent: "center",
-                                placeContent: "center",
-                            }}
-                        >
-                            <div>
-                                <button
-                                    style={{
-                                        paddingTop: 15,
-                                        paddingLeft: 25,
-                                        paddingBottom: 15,
-                                        paddingRight: 25,
-                                        backgroundColor: "#00002e",
-                                        border: "2px solid grey",
-                                        borderRadius: 5,
-                                    }}
-                                    onClick={() => {
-                                        const audio = document.getElementById(
-                                            "errorsong",
-                                        )! as HTMLAudioElement;
-                                        audio.play();
-                                        document.getElementById(
-                                            "errorPage",
-                                        )!.style.display = "flex";
-                                        document.getElementById(
-                                            "buttonTriggerPlay",
-                                        )!.style.display = "none";
-                                        document.title = "Error404";
-                                        document.head.ariaDescription =
-                                            "PUUUUUUUTZZZZZZ.... PaGiNa NãO EnCoNtRaDa";
-                                        let novoIcon =
-                                            document.createElement("link");
-                                        novoIcon.rel = "icon";
-                                        novoIcon.href = "/gifs/nyan_cat.gif";
-                                        document.head.appendChild(novoIcon);
-                                    }}
-                                >
-                                    Clique Me
-                                </button>
-                            </div>
-                        </div>
-                        <div id="errorPage">
-                            <div>
-                                <Image
-                                    src={"/gifs/nyan_cat.gif"}
-                                    width={150}
-                                    height={150}
-                                    alt="ErrorGif"
-                                    style={{
-                                        borderRadius: 99,
-                                    }}
-                                />
-                            </div>
-                            <div>
-                                <h1>Error404 Page Not Found</h1>
-                                <p>
-                                    PUUUUUUUTZZZZZZ.... Deu ERRO AqUi oh Pagina
-                                    NãO EnCoNtRaDa, isso quer dizer que você
-                                    entrou em uma pagina que não existe nos
-                                    arquivos do meu site
-                                </p>
-                            </div>
+                        <div>
+                            <button
+                                style={{
+                                    paddingTop: 15,
+                                    paddingLeft: 25,
+                                    paddingBottom: 15,
+                                    paddingRight: 25,
+                                    backgroundColor: "#00002e",
+                                    border: "2px solid grey",
+                                    borderRadius: 5,
+                                }}
+                                onClick={() => {
+                                    const audio = document.getElementById(
+                                        "errorsong",
+                                    )! as HTMLAudioElement;
+                                    audio.play();
+                                    document.getElementById(
+                                        "errorPage",
+                                    )!.style.display = "flex";
+                                    document.getElementById(
+                                        "buttonTriggerPlay",
+                                    )!.style.display = "none";
+                                    document.title = "Error404";
+                                    document.head.ariaDescription =
+                                        "PUUUUUUUTZZZZZZ.... PaGiNa NãO EnCoNtRaDa";
+                                    let novoIcon =
+                                        document.createElement("link");
+                                    novoIcon.rel = "icon";
+                                    novoIcon.href = "/gifs/nyan_cat.gif";
+                                    document.head.appendChild(novoIcon);
+                                }}
+                            >
+                                Clique Me
+                            </button>
                         </div>
                     </div>
-                </body>
-            </>
-        );
-    }
+                    <div id="errorPage">
+                        <div>
+                            <Image
+                                src={"/gifs/nyan_cat.gif"}
+                                width={150}
+                                height={150}
+                                alt="ErrorGif"
+                                style={{
+                                    borderRadius: 99,
+                                }}
+                            />
+                        </div>
+                        <div>
+                            <h1>Error404 Page Not Found</h1>
+                            <p>
+                                PUUUUUUUTZZZZZZ.... Deu ERRO AqUi oh Pagina NãO
+                                EnCoNtRaDa, isso quer dizer que você entrou em
+                                uma pagina que não existe nos arquivos do meu
+                                site
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </body>
+        </>
+    );
+}
+
+export async function getServerSideProps(context: any) {
+    const response = await axios
+        .get("/api/teste")
+        .then((x) =>
+            x.data.response.find((i: any) => i.id === context.query.id),
+        )
+        .catch((err) => console.error(err));
+
+    if (response)
+        return {
+            props: response,
+        };
+    return { notFound: true };
 }
